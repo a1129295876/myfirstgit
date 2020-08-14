3335ea3bed15bc45039082e7833628ec3b28d1c6
yepeng <11292@qq.com>
2020-08-14 19:52:46 +0800

    test提交

--- fitstgit/src/demo1.java
+++ fitstgit/src/demo1.java
@@ -3,8 +3,6 @@ public class demo1 {
     public void addUser(){
         System.out.println("welcome to Git");
         System.out.println("第二次提交");
-        System.out.println("d");
-        System.out.println("第三次提交");
-        System.out.println("123");
+
     }
 }
