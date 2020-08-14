af99c12a84a92d69b0f9864f3c84370be7d64b7f
yepeng <11292@qq.com>
2020-08-14 21:54:05 +0800

    test1提交
    # Conflicts:
    #	fitstgit/src/demo1.java

--- fitstgit/src/demo1.java
+++ fitstgit/src/demo1.java
@@ -2,18 +2,12 @@ public class demo1 {
 
     public void addUser(){
         System.out.println("welcome to Git");
-        System.out.println("第二次提交");
-        System.out.println("d");
-        System.out.println("第三次提交");
-        System.out.println("123");
-        System.out.println("这条是我在github上修改的");
+
 
 
     }
 
     public void fun2(){
-        System.out.println("fun2");
-         System.out.println("GitHub修改了这里");
-        System.out.println("先commit");
+
     }
 }
